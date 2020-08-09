28e4547dfd939c79c96b7bcb755e07f63dac2d50
MikeDRNV <MikeDRNV@gmail.com>
2020-08-09 21:45:27 +0300

    Create PanelOne.java

---
+++ Lina/src/company/PanelOne.java
@@ -0,0 +1,143 @@
+package company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class PanelOne extends JPanel {
+    private JButton[] numbers = new JButton[10];
+    private Font font = new Font("SanSerif", Font.ITALIC, 20); // шрифт (Font - класс для создания шрифтов)
+    private JTextField output = new JTextField();
+    private JButton backspace = new JButton("<"), equ = new JButton("=");
+    private JButton plus = new JButton("+"), minus = new JButton("-"),
+            multi = new JButton("*"), div = new JButton("/");
+    int firstValue = 0;
+    String operation = "+";
+    public PanelOne(){
+        setLayout(null); // размещать элементы так как захочется
+        setFocusable(true);// панель забирает себе фокус
+        grabFocus();
+
+        plus.setBounds(190,70,50,50);
+        plus.setFont(font);
+        add(plus);
+
+        minus.setBounds(190,130,50,50);
+        minus.setFont(font);
+        add(minus);
+
+        multi.setBounds(190,190,50,50);
+        multi.setFont(font);
+        add(multi);
+
+        div.setBounds(190,250,50,50);
+        div.setFont(font);
+        add(div);
+
+        equ.setBounds(130,250,50,50);
+        equ.setFont(font);
+        add(equ);
+
+        backspace.setBounds(10,250,50,50);
+        backspace.setFont(font);
+        add(backspace);
+
+        numbers[0] = new JButton( "0");
+        numbers[0].setBounds(70, 250, 50, 50); // задать размер
+        numbers[0].setFont(font);
+        add(numbers[0]);
+
+        for(int x=0; x<3; x++){
+            for(int y=0; y<3; y++){
+                numbers[x*3+y+1] = new JButton((x*3+y+1) + "");
+                numbers[x*3+y+1].setBounds(x*(50+10)+10, y*(50+10)+70, 50, 50); // задать размер
+                numbers[x*3+y+1].setFont(font);
+                add(numbers[x*3+y+1]);
+            }
+        }
+        output.setBounds(10,10,235,50);
+        output.setFont(font);
+        output.setEditable(false); // не можем изменять текст в окне
+        add(output);
+
+        ActionListener l = (ActionEvent e) -> {
+            JButton b = (JButton) e.getSource();// возвращает обьект который вызвал этот метод
+            output.setText(output.getText() + b.getText());
+        }; // лямда выражение
+        for(JButton b : numbers){
+            b.addActionListener(l);
+        }
+        addKeyListener(new KeyAdapter(){
+            @Override
+            public void keyPressed(KeyEvent e){
+                char symbol = e.getKeyChar();
+                if(!Character.isDigit(symbol))
+                    return;
+
+                output.setText(output.getText() + symbol);
+            }
+        });
+
+        backspace.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String temp = output.getText();
+                output.setText(temp.substring(0,temp.length()-1));
+            }
+        });
+        plus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstValue = Integer.valueOf(output.getText());
+                output.setText("");
+                operation = "+";
+            }
+        });
+        multi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstValue = Integer.valueOf(output.getText());
+                output.setText("");
+                operation = "*";
+            }
+        });
+        div.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstValue = Integer.valueOf(output.getText());
+                output.setText("");
+                operation = "/";
+            }
+        });
+        minus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstValue = Integer.valueOf(output.getText());
+                output.setText("");
+                operation = "-";
+            }
+        });
+        equ.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int secondValue = Integer.valueOf(output.getText());
+                if("+".equals(operation)){
+                    output.setText(firstValue + secondValue + "");
+                } else if("-".equals(operation)){
+                    output.setText((firstValue - secondValue) + "");
+                } else if("*".equals(operation)){
+                    output.setText((firstValue * secondValue) + "");
+                } else if("/".equals(operation)){
+                    if(secondValue == 0){output.setText("I N F I N I T Y");}
+                    else
+                        output.setText((firstValue / secondValue) + "");
+                }
+                firstValue = 0;
+                operation = "+";
+            }
+        });
+    }
+}
